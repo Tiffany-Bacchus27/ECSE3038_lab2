51f3bdaf08c7be366bd81b32c4aa818e3d988344
Tiffany-Bacchus27 <tbacchus1997@gmail.com>
2025-02-07 14:07:04 -0500

    added file to repo

--- app.py
+++ app.py
@@ -4,30 +4,35 @@ from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
-data = [
-    {"name": "Sam Holland", "occupation": "Librarian", "age": 28},
-    {"name": "Alex Jordan", "occupation": "Graphic Artist", "age": 26},
-    {"name": "Clover Butler", "occupation": "Fashion Designer", "age": 27}
-]
+
+data = []
+
 
 class Person(BaseModel):
     name: str
     occupation: str
-    age: int
+    address: str
 
 @app.post("/person")
 async def add_person(person_request: Person):
-   
-    if not person_request.name or not person_request.occupation or not person_request.age:
-        return {"success": False, "result": {"error_message": "invalid request"}}
+    
+    if not person_request.name or not person_request.occupation or not person_request.address:
+        return {
+            "success": False,
+            "result": {
+                "error_message": "invalid request"
+            }
+        }
+    
     
     person_json = jsonable_encoder(person_request)
-    data.append(person_json) 
-
-    return {"success": True, "result": person_json}
+    data.append(person_json)
+    
+    return {
+        "success": True,
+        "result": person_json
+    }
 
 @app.get("/person")
 async def get_people():
     return data
-
-    
